81b1fe4d8319724e2d4c82a35ae2f28156ee27d0
Enma Leticia Ramírez Castro <eramirezc38@miumg.edu.gt>
2024-03-02 11:25:55 -0600

    Parcial

---
+++ Parcial programacion/NumPares.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numpareseimpares
+{
+    internal class NumPares
+    {
+        internal class paresImpares
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Introduce Los numeros:");
+                int CantidadNumeros = int.Parse(Console.ReadLine());
+
+                int[] numeros = new int[CantidadNumeros];
+
+                for (int i = 0; i < CantidadNumeros; i++)
+                {
+                    Console.WriteLine("Introduce el numero:");
+                    numeros[i] = int.Parse(Console.ReadLine());
+
+                }
+                int cantidadPares = 0, cantidadImpares = 0;
+                int sumaPares = 0, sumaImpares = 0;
+
+                foreach (int numero in numeros)
+                {
+                    if (numero % 2 == 0)
+                    {
+                        cantidadPares++;
+                        sumaPares += numero;
+                    }
+                    else
+                    {
+                        cantidadImpares++;
+                        sumaImpares += numero;
+                    }
+                }
+                Console.WriteLine($"Cantidad de numeros pares:{cantidadPares}");
+                Console.WriteLine($"Cantidad de numeros Impares:{cantidadImpares}");
+                Console.WriteLine($"Siuma de pares:{sumaPares}");
+                Console.WriteLine($"DSuma de impares:{sumaImpares}");
+            }
+        }
+    }
+}
---
+++ Parcial programacion/calculadora.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class calculadora
+    {
+        public int Sumar(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+
+        }
+        public int Restar(int numero1, int numero2)
+        {
+            return numero1 - numero2;
+
+        }
+        public int Multiplicar(int numero1, int numero2)
+        {
+            return numero1 * numero2;
+
+        }
+        public int Dividir(int numero1, int numero2)
+        {
+            if (numero2 != 0)
+            {
+                return numero1 / numero2;
+            }
+            else
+            {
+                Console.WriteLine("Error: No se puede dividir por cero: ");
+                return 0;
+            }
+
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            calculadora MisOperacionesMatematicas = new calculadora();
+            int numero1 = 46;
+            int numero2 = 80;
+
+            Console.WriteLine($"Suma:{MisOperacionesMatematicas.Sumar(numero1, numero2)}");
+            Console.WriteLine($"Resta:{MisOperacionesMatematicas.Restar(numero1, numero2)}");
+            Console.WriteLine($"Multiplicacion:{MisOperacionesMatematicas.Multiplicar(numero1, numero2)}");
+            Console.WriteLine($"Divicion:{MisOperacionesMatematicas.Dividir(numero1, numero2)}");
+
+        }
+    }
+
+
+
+}
+
+
+    
+
+
+    
+
