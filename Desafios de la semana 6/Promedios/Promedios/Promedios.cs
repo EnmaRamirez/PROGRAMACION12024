dbb5b7114b2b48c8bf8aaa6283c5cafd576762d9
Enma Leticia Ramírez Castro <eramirezc38@miumg.edu.gt>
2024-03-15 14:09:48 -0600

    Agregando carpeta semana 6

---
+++ Desafios de la semana 6/Promedios/Promedios/Promedios.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedios
+{
+    internal class Promedios
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese los puntajes de su alumno (validos entre 1 a 10), escriba 'fin' cuando termine:");
+
+            double suma = 0;
+            int contador = 0;
+            bool finalizar = false;
+
+            while (!finalizar)
+            {
+                string input = Console.ReadLine();
+
+                if (input.ToLower() == "fin")
+                {
+                    finalizar = true;
+                }
+                else
+                {
+                    if (double.TryParse(input, out double puntaje))
+                    {
+                        if (puntaje >= 1 && puntaje <= 10)
+                        {
+                            suma += puntaje;
+                            contador++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("El puntaje debe estar entre 1 y 10.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Inválido. Por favor, ingrese un número válido.");
+                    }
+                }
+            }
+
+            if (contador > 0)
+            {
+                double promedio = suma / contador;
+                Console.WriteLine($"El puntaje promedio del alumno es: {promedio}");
+            }
+            else
+            {
+                Console.WriteLine("No existen puntajes validos.");
+            }
+        }
+
+    }
+}
---
+++ Desafios de la semana 6/TotalVolumenCajas/TotalVolumenCajas/Caja.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalVolumenCajas
+{
+    public class Caja
+    {
+        public double Largo { get; set; }
+        public double Ancho { get; set; }
+        public double Alto { get; set; }
+
+        public double CalcularVolumen()
+        {
+            return Largo * Ancho * Alto;
+
+        }
+        static void Main(string[] args)
+        {
+            List<Caja> cajas = new List<Caja>();
+
+            Console.WriteLine("Ingrese las dimensiones de las cajas (largo, ancho, alto):");
+            while (true)
+            {
+                Console.WriteLine("Caja #{0}:", cajas.Count + 1);
+                double largo = Convert.ToDouble(Console.ReadLine());
+                double ancho = Convert.ToDouble(Console.ReadLine());
+                double alto = Convert.ToDouble(Console.ReadLine());
+
+                Caja nuevaCaja = new Caja { Largo = largo, Ancho = ancho, Alto = alto };
+                cajas.Add(nuevaCaja);
+
+                Console.WriteLine("¿Desea agregar otra caja? (si/no)");
+                if (Console.ReadLine().ToLower() != "si")
+                    break;
+            }
+            double volumenTotal = 0;
+            foreach (var caja in cajas)
+            {
+                volumenTotal += caja.CalcularVolumen();
+            }
+
+            double volumenPromedio = volumenTotal / cajas.Count;
+
+            Console.WriteLine("Volumen total de las cajas: " + volumenTotal);
+            Console.WriteLine("Volumen promedio de las cajas: " + volumenPromedio);
+
+        }
+    }
+}
